76c8abd32617f8aac01971ef442d2b83ea458fb8
donnchao <donnchao@outlook.com>
2020-12-05 15:28:37 +0800

    3.6-1 ref

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <h1>{{ count }}</h1>
+  <h3>{{ double }}</h3>
+  <button @click="increase">^=>+1</button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { ref, computed, reactive,toRefs } from "vue";
+
+export default defineComponent({
+  name: "App",
+  setup() {
+    // ref一般使用原始类型
+    const count = ref(0);
+    const increase = () => {
+      count.value++;
+    }
+    const double = computed(()=>{
+      return count.value * 2;
+    });
+    return {
+      // 需要导出后才能在模板中使用
+      count, increase, double
+    };
+  },
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+/**
+ * Vue3新特性：
+ * Composition API：
+ *  ref reactive
+ *  computed watch
+ *  新的生命周期函数
+ *  自定义函数和 - Hooks函数
+ * 
+ * 其他新特性：
+ *  Teleport - 瞬移组件的位置
+ *  Suspense - 异步加载组件的新福音
+ *  全局API的修改和优化
+ *  更多的试验性特性
+ */
+createApp(App).mount('#app')
