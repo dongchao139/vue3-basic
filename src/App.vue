fef67ede7e3b072c473da87705c880f645d0f591
donnchao <donnchao@outlook.com>
2020-12-05 15:43:11 +0800

    3.7 响应式原理：Proxy

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,63 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <h1>{{ count }}</h1>
-  <h4>{{ double }}</h4>
+  <h1>
+    <span>{{ count }}</span> - <span>{{ double }}</span>
+  </h1>
   <button @click="increase">^=>+1</button>
+  <ul>
+    <li v-for="number in numbers" :key="number">
+      <h1>{{ number }}</h1>
+    </li>
+  </ul>
+  <h4>{{ person.name }}</h4>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import { ref, computed, reactive,toRefs } from "vue";
+import { ref, computed, reactive, toRefs } from "vue";
 
 interface DataProps {
   count: number;
   increase: () => void;
   double: number;
+  numbers: number[];
+  person: { name?: string };
 }
 
+/**
+ * 响应式对象原理
+ * Vue2.x
+ *  Object.defineProperty(data, 'count', {
+ *    get() {},
+ *    set() {}
+ *  });
+ *
+ * Vue3.0
+ *  new Proxy(data, {
+ *    get(key) {},
+ *    set(key, value) {}
+ *  })
+ */
 export default defineComponent({
   name: "App",
   setup() {
-
     const data: DataProps = reactive({
-      count: 0, 
-      increase: () =>  {
+      count: 0,
+      increase: () => {
         data.count++; // 这里不用.value
       },
-      double: computed(()=>{
+      double: computed(() => {
         return data.count * 2;
-      })
+      }),
+      numbers: [0, 1, 2],
+      person: {},
     });
+    data.numbers[0] = 5;
+    data.person.name = "viking";
     // 将响应式对象转换为ref对象
     const refData = toRefs(data);
     return {
-      ...refData
+      ...refData,
     };
   },
 });
@@ -45,5 +71,11 @@ export default defineComponent({
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+ul {
+  padding-left: 0;
+}
+li {
+  list-style: none;
 }
 </style>
