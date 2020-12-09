4a76371cfe69531be7f7542a8767827e230ce304
donnchao <donnchao@outlook.com>
2020-12-09 21:55:44 +0800

    3-17 Suspense

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,20 +4,34 @@
   <h3>{{greetings}}</h3>
   <button @click="upateGreeting">greeting</button>
   <h3>x: {{x}} y: {{y}}</h3>
-
-  <h3 v-if="loading">Loading...</h3>
+  <Suspense>
+    <!--
+    <Suspense> slots expect a single root node.
+    -->
+    <template #default>
+      <!-- <async-show /> -->
+      <dog-show />
+    </template>
+    <template #fallback>
+      <h1>content is loading</h1>
+    </template>
+  </Suspense>
+  <p>{{err}}</p>
+  <!-- <h3 v-if="loading">Loading...</h3>
   <img v-if="loaded" :src="result.message">
-  <modal :isOpen="open" @close-modal="onModalClose">My Modal!!!</modal>
+  <modal :isOpen="open" @close-modal="onModalClose">My Modal!!!</modal> -->
 </template>
 
 <script lang="ts">
 import { 
   defineComponent, onMounted, onUpdated, 
-  reactive, ref,watch, onUnmounted
+  reactive, ref,watch, onUnmounted, onErrorCaptured
 } from "vue";
 import useMousePosition from '../hooks/useMousePosition';
 import useURLLoader from '../hooks/useURLLoader';
 import Modal from "./Modal.vue";
+import AsyncShow from './AsyncShow.vue';
+import DogShow from './DogShow.vue';
 /**
  * Vue3 生命周期
  * beforeCreate  -> use setup()
@@ -40,7 +54,11 @@ interface DogResult {
   status: string;
 }
 export default defineComponent({
-  components:{ Modal },
+  components:{ 
+    // Modal,
+    DogShow,
+    // AsyncShow
+  },
   setup() {
     onUpdated(() => {
       console.log("updated");
@@ -65,17 +83,24 @@ export default defineComponent({
     });
     const {x, y} = useMousePosition();
 
-    const {loading,loaded,result,error} = 
+    const {loading, loaded, result, error} = 
       useURLLoader<DogResult>('https://dog.ceo/api/breeds/image/random');
     watch(result, () => {
       if (result && result.value) {
         console.log(result.value.message);
       }
     });
-    const open = ref(true);
+    const open = ref(false);
     const onModalClose = () => {
       open.value = false;
     }
+
+    const err = ref(null);
+    onErrorCaptured((e: any) => {
+      err.value = e;
+      return true;
+    })
+
     return {
       count,
       greetings,
@@ -83,7 +108,8 @@ export default defineComponent({
       onClick,
       x, y,
       loading, loaded, result, error,
-      open, onModalClose
+      open, onModalClose,
+      err
     };
   },
 });
