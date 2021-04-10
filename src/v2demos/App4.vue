5d93fb6d69428ded7ed4d9ed846a6d9dbe85041e
董超139 <donnchao@outlook.com>
2021-04-10 17:21:44 +0800

    delete

--- src/v2demos/App4.vue
+++ src/v2demos/App4.vue
@@ -3,15 +3,26 @@
     <h1 v-show="show">hello world</h1>
     <button v-on:click="handleClick">toggle</button>
     <ul>
-      <li v-for="item of list" :key="item">{{item}}</li>
+      <TodoItem 
+        v-for="(item,key) of list" :key="item" v-bind:item="item"
+        v-on:delete-item="handleDelete(key)"
+      >
+        {{item}}
+      </TodoItem>
     </ul>
     <button v-on:click="addItem">addItem</button>
   </div>
 </template>
 
-<script lang="js">
-export default {
+<script lang="ts">
+import TodoItem from './TodoItem.vue';
+import {defineComponent} from 'vue';
+
+export default defineComponent({
   name: "App1",
+  components: {
+    TodoItem
+  },
   data() {
     return {
       show: true,
@@ -25,9 +36,12 @@ export default {
     addItem() {
       const newValue = this.list.length + 1;
       this.list[newValue - 1] = newValue;
+    },
+    handleDelete(key: number) {
+      this.list.splice(key, 1);
     }
   }
-}
+})
 </script>
 
 <style scoped>
---
+++ src/v2demos/TodoItem.vue
@@ -0,0 +1,14 @@
+<template>
+  <li @click="handleClick">{{ item }}-<slot></slot></li>
+</template>
+
+<script lang="ts">
+export default {
+  props: ["item"],
+  methods: {
+    handleClick() {
+      this.$emit("delete-item", this.item);
+    },
+  },
+};
+</script>
